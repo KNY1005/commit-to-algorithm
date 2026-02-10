ac803799b66b9bdf5f1e03c7251ffeab56fde07e
김나영 <111120709+KNY1005@users.noreply.github.com>
2026-02-10 10:44:02 +0900

    [김니영] 01.29 해시(2) 문제 재 제출 (#28)
    * docs : 0122 빈파일 삭제
    
    * solve : 문제 8 ~ 13번 정답 및 문제 9번 추가
    
    * docs : pull request 템플릿 수정
    
    * refactor : [문제09] 코드 개선
    
    * solev : 문제 16, 17 정답
    
    * solve : 15번 푸는 중..
    
    * docs : 0128 해시(1) 문제 업로드
    
    * solve :  문제 18, 19, 20 정답
    
    * solve : 21~23 문제 정답
    
    * dose :  정답답 파일 생성
    
    * solve : 해시(2) 정답
    
    * solve : 25번 문제 재 제출

--- solutions/nayoung/0129/[문제25]베스트앨범.js
+++ solutions/nayoung/0129/[문제25]베스트앨범.js
@@ -1,3 +1,84 @@
+
+// 주석 없는 버전
+function solution(genres, plays) {
+  const answer = []; 
+  
+  const music = genres.reduce((arr, cur, index) => {
+    if (!arr[cur]) {
+      arr[cur] = { total: 0, song: [] };
+    }
+    arr[cur].song.push([index, plays[index]]);
+    arr[cur].total += plays[index];
+    return arr;
+  }, {});
+
+  const genreList = Object.keys(music).sort((a, b) => {
+    return music[b].total - music[a].total;
+  });
+
+  for (const genre of genreList) {
+    music[genre].song.sort((a, b) => {
+      if (b[1] !== a[1]) return b[1] - a[1];
+      return a[0] - b[0];
+    });
+
+    answer.push(music[genre].song[0][0]);
+    if (music[genre].song.length > 1) {
+      answer.push(music[genre].song[1][0]);
+    }
+  }
+
+  return answer;
+}
+
+//----------------------------------------------
+// 주석 있는 버전
+
+function solution(genres, plays) {
+  const answer = []; 
+  
+  // 1. reduce로 객체 생성 후 장르별 구조 정의
+  // reduce 사용
+  const music = genres.reduce((arr, cur, index) => {
+    // 해당 장르가 없으면 객체 생성
+    if(!arr[cur]){
+      arr[cur] = {
+        total : 0,
+        song : []
+      }
+    }
+    // song 안에 고유번호, 재생수 넣기
+    arr[cur].song.push([index, plays[index]])
+    // 장르별 총 재생 수 누적
+    arr[cur].total += plays[index];
+    return arr;
+  }, {}); // 의존성 까먹지 말기
+
+  // 2. 총 재생 수 많은 장르부터 정렬
+  // Object.keys(music)을 해주면 객체에서 배열로 바꿀 수 있음!
+  // a, b는 장르이름 => music[b].total = 해당 장르의 총 재생수 
+  const genreList = Object.keys(music).sort((a, b) => {
+    return music[b].total - music[a].total
+  })
+  
+  // 3. music객체 안에 있는 모든 장르를 하나씩 꺼내서 반복한다
+  for(const genre of genreList){
+    // 재생수 내림차순, 같으면 고유번호 오름차순
+    music[genre].song.sort((a, b) => {
+      // 이해하기 힘들었던 부분
+      // 재생수 제일 큰걸 맨 앞으로 정렬
+      if (b[1] !== a[1]) return b[1] - a[1];
+      // 재생수가 같을 경우 인댁스 작은게 앞으로
+      return a[0] - b[0];
+    });
+
+    // 장르별 제일 인기 있는 곡 고유번호 넣기
+    answer.push(music[genre].song[0][0]);
+
+    // 근데 노래의 갯수가 1개 이상이면 두번째 곡도 넣기
+    if(music[genre].song.length > 1){
+      answer.push(music[genre].song[1][0])
+=======
 // 해당 문제는 못풀겠어서 gpt에게 강습?받은 코드입니다
 
 
@@ -73,3 +154,4 @@ function solution(genres, plays) {
 
   return answer; // 최종 결과 반환
 }
+
